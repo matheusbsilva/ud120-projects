ffab358960756d833640acf6834d825d1fd79876
matheusbsilva <matheusbattista@hotmail.com>
2019-01-09 09:24:25 -0300

    implement fitting and acc measure of naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
+from sklearn.naive_bayes import GaussianNB
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -21,13 +24,13 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+tf = round(time()-t0, 3)
 
+acc = clf.score(features_test, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print('Accuracy of Naive Bayes Gaussian:', acc)
+print('Total time(sec) for fitting the model:', tf)

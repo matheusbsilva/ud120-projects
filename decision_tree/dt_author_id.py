aacf2f26ffdd20d4b96bb16c7e0f91df98dcff02
matheusbsilva <matheusbattista@hotmail.com>
2019-01-09 10:07:07 -0300

    implement decision tree classifier with min sample split of 40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
+from sklearn.tree import DecisionTreeClassifier
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -19,13 +22,10 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf_40_split = DecisionTreeClassifier(min_samples_split=40)
+clf_40_split.fit(features_train, labels_train)
 
+acc_40_split = clf_40_split.score(features_test, labels_test)
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print('Accuracy of Decision Tree Classifier with min samples split of 40:',
+      acc_40_split)
